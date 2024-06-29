4d1e8bd7932fe558bc23730d4c2aff939a8c224e
PanchalYashh <panchalyash914@gmail.com>
2024-06-29 12:24:01 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import RegisterPage from './Pages/RegisterPage';
+import LoginPage from './Pages/LoginPage';
+import Register from './Components/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/RegisterPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Register from '../Components/Register'
+export default function RegisterPage() {
+  return (
+    <>
+        <Register></Register>
+    </>
+    
+
+  )
+}
